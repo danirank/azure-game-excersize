303cc87eaafd7c134e6bc42083502183498c01a4
Kevin Hermansson <hermanssonkevin@gmail.com>
2026-08-11 13:12:23 +0200

    Add QuickGame project

---
+++ QuickGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+Random random = new Random();
+int secretNumber = random.Next(1, 101);
+int guess = 0;
+int numberOfGuess = 0;
+
+Console.WriteLine("Welcome to Guesst the number");
+Console.WriteLine("I'm thinking of a number from 1 - 100.");
+
+while (guess != secretNumber)
+{
+    Console.Write("Write your guess: ");
+
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out guess))
+    {
+        numberOfGuess++;
+
+        if (guess < secretNumber)
+        {
+            Console.WriteLine("Too low! Try again.");
+        }
+        else if (guess > secretNumber)
+        {
+            Console.WriteLine("Too high! Try again.");
+        }
+        else
+        {
+            Console.WriteLine($"Congrats! You made it on {numberOfGuess} tries.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Wrong format, write a number.");
+    }
+}
